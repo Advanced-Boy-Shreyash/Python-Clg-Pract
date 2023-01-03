959b4543b408a5a234752bffea7838ad12c88e04
Shreyash <shreyashnandurkar2@gmail.com>
2023-01-03 10:41:35 +0530

    Added Clg Pract Progs

---
+++ Prog1.py
@@ -0,0 +1,44 @@
+# num=1
+# even_num=[]
+# while num<=100:
+#     if num%2==0:
+#         even_num.append(num)
+#     num+=1
+# print(even_num)
+
+# i=True
+# while i:
+#     print ("Coondition Satisfied")
+
+# while (num<10):
+#     even_num.append(num)
+#     num+=1
+# print(even_num)
+
+# for i in range(1,100):
+#     if i%1==0:
+#         even_num.append(i)
+#     num+=1
+# print(even_num)
+
+# iterator=[1,2,3,4,5]
+# for item in iterator:
+#     print (item)
+# else:
+#     print("no more items in iterator")
+
+# a in iterator
+
+# ex={
+#     'iter':'dict',
+#     'loop':'whille',
+#     'operation':'dde'
+# }
+# print(ex)
+
+# even_num=[]
+# for items in range(1,11):
+#     if items%2==0:
+#         even_num.append(items)
+#         items+=1
+#     print(even_num)
